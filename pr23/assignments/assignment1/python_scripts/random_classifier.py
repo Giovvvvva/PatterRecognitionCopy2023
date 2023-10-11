25ba611745081dc3bb220c671d89d1a72e4e963e
Giovanni <utzi98@gmail.com>
2023-10-11 18:59:46 +0200

    "Load Repo from PC onto git" :qw

---
+++ pr23/assignments/assignment1/python_scripts/random_classifier.py
@@ -0,0 +1,36 @@
+'''
+This contains the skeleton code for the random classifier
+exercise.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from perceptron import MutiClassPerceptron
+
+
+
+
+if __name__ == '__main__':
+
+    # Generate random data
+
+    N = 100
+    p = 0.5
+    d_init = 10
+
+    # Let #X_train = N x d abd #y_train = N x 1 be the training data
+    # Using random.randn() generate X_train and random.binomial() generate y_train
+    # make sure to one-hot-encode y_train
+    # similarly generate X_test and y_test
+    # make sure to one-hot-encode y_test
+    # Initialize the perceptron with W_int = np.zeros((d,2)) and train it
+    # Repeat the above steps for d = np.arange(10,200,10)
+    # Plot the accuracy on the training and test set as a function of d
+
+
+
+
+
+
+
+
